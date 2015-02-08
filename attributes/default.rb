148194bd925800821a81733f28f90510c1f6a4fa
Léonard TAVAE <leonard.tavae@informatique.gov.pf>
2015-02-07 23:36:29 -1000

    Modification majeure du code de base.

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default['dsi-users']['dev'] = false
+default['dsi-users']['databag']['name'] = 'users'
+default['dsi-users']['databag']['encrypted'] = true
--- definitions/dsi_users.rb
+++ definitions/dsi_users.rb
@@ -1,14 +1,45 @@
-define :dsi_users, username: nil, password: nil do # ~FC015
+define :dsi_users, username: nil, password: nil, passwordless: false do # ~FC015
 
-  user params[:username] do
+  if node['dsi-users']['databag']['encrypted'] == true
+    secret = Chef::EncryptedDataBagItem.load_secret('/tmp/kitchen/encrypted_data_bag_secret')
+    item =  Chef::EncryptedDataBagItem.load(node['dsi-users']['databag']['name'], params[:name], secret)
+  else
+    item = data_bag_item(node['dsi-users']['databag']['name'], params[:name])
+  end
+
+  home_directory = "/home/#{params[:name]}"
+  if params[:password].to_s == ''
+    user_password = item['password']
+  else
+    user_password = params[:password]
+  end
+
+  user params[:name] do
     supports manage_home: true
-    comment "#{params[:username]} User"
-    home "/home/#{params[:username]}"
+    comment "#{params[:name]} User"
+    home home_directory
     shell '/bin/bash'
-    password params[:password]
+    password user_password
   end
 
-  sudo params[:username] do
-    user params[:username]
+  sudo params[:name] do
+    user params[:name]
+    nopasswd params[:passwordless]
   end
+
+  directory "#{home_directory}/.ssh" do
+    owner params[:name]
+    group params[:name]
+    mode 0700
+    action :create
+    only_if "test -d #{home_directory}"
+  end
+
+  template "#{home_directory}/.ssh/authorized_keys" do
+    source 'authorized_keys.erb'
+    owner params[:name]
+    group params[:name]
+    variables ssh_keys: item['ssh_keys']
+  end
+
 end
